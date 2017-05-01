14608498147c54dde74fa9ef9808f4d5da176d41
Luke Blaney <git@lukeblaney.co.uk>
2017-05-01 21:25:29 +0100

    add handler for fingerprint endpoint

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
+}
+func fingerprint(w http.ResponseWriter, r *http.Request) {
+	id := strings.Replace(r.URL.Path, "/fingerprint/", "", 1)
+	switch r.Method {
+		case "GET":
+			io.WriteString(w, "Hello fingerprint! "+id)
+		case "PUT":
+			io.WriteString(w, "PUT not done yet "+id)
+		default:
+			w.Header().Set("Allow", "GET, PUT")
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			io.WriteString(w, "Error ")
+	}
 }
 
 func main() {
@@ -18,6 +32,7 @@ func main() {
 	} else {
 		port = "8080"
 	}
+	http.HandleFunc("/fingerprint/", fingerprint)
 	http.HandleFunc("/", hello)
 	log.Printf("Listen on port %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
