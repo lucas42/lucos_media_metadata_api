a03017b228e1cd181c9c6f4185169f33864818b5
Luke Blaney <git@lukeblaney.co.uk>
2023-03-18 14:02:19 +0000

    only send track or url to loganne when they're actually relevant

--- src/loganne.go
+++ src/loganne.go
@@ -20,17 +20,23 @@ type Loganne struct {
 func (loganne Loganne) post(eventType string, humanReadable string, updatedTrack Track, existingTrack Track) {
 	url := loganne.host + "/events"
 
-	track := updatedTrack
-	if track.ID == 0 {
-		track = existingTrack
-	}
-	postData, _ := json.Marshal(map[string]interface{}{
+	data := map[string]interface{}{
 		"source":  loganne.source,
 		"type": eventType,
 		"humanReadable": humanReadable,
-		"track": track,
-		"url": fmt.Sprintf("https://media-metadata.l42.eu/tracks/%d", track.ID), // Included for all events, but perhaps less helpful for delete events as the URL will 404?
-	})
+	}
+
+	// If there's an updated track, include that in the data and link to it in loganne
+	if updatedTrack.ID > 0 {
+		data["track"] = updatedTrack
+		data["url"] = fmt.Sprintf("https://media-metadata.l42.eu/tracks/%d", updatedTrack.ID)
+
+	// If the was an existing track, but no updated one (ie a delete event)
+	// include the old track in the data, but don't link to it as the link will 404
+	} else if existingTrack.ID > 0 {
+		data["track"] = existingTrack
+	}
+	postData, _ := json.Marshal(data)
 	_, err := http.Post(url, "application/json", bytes.NewBuffer(postData))
 	if err != nil {
 		log.Printf("Error occured whilst posting to Loganne %v", err)
--- src/loganne_test.go
+++ src/loganne_test.go
@@ -66,13 +66,30 @@ func TestLoganneDeleteEvent(test *testing.T) {
 	}
 
 	// Delete event passes empty track for the updated track, then existing track
-	loganne.post("deleteEvent", "This event is from the test", Track{}, track)
+	loganne.post("deleteEvent", "This event is from the delete test", Track{}, track)
 
 	assertEqual(test, "Loganne request made to wrong path", "/events", latestRequest.URL.Path)
 	assertEqual(test,"Loganne request wasn't POST request", "POST", latestRequest.Method)
 
 	assertNoError(test, "Failed to get request body", latestRequestError)
-	assertEqual(test, "Unexpected request body", `{"humanReadable":"This event is from the test","source":"metadata_api_test","track":{"fingerprint":"abc","duration":137,"url":"http://example.com/track-1.mp3","trackid":18,"tags":{"album":"Harvest Storm","artist":"Altan"},"weighting":9},"type":"deleteEvent","url":"https://media-metadata.l42.eu/tracks/18"}`, latestRequestBody)
+	assertEqual(test, "Unexpected request body", `{"humanReadable":"This event is from the delete test","source":"metadata_api_test","track":{"fingerprint":"abc","duration":137,"url":"http://example.com/track-1.mp3","trackid":18,"tags":{"album":"Harvest Storm","artist":"Altan"},"weighting":9},"type":"deleteEvent"}`, latestRequestBody)
+}
+
+/**
+ * Test loganne for an event which modifies multiple tracks
+ **/
+func TestBulkEvent(test *testing.T) {
+	loganne := Loganne{
+		host: "http://localhost:7999",
+		source: "metadata_api_test",
+	}
+	loganne.post("bulkTestEvent", "This event is from the bulk test", Track{}, Track{})
+
+	assertEqual(test, "Loganne request made to wrong path", "/events", latestRequest.URL.Path)
+	assertEqual(test,"Loganne request wasn't POST request", "POST", latestRequest.Method)
+
+	assertNoError(test, "Failed to get request body", latestRequestError)
+	assertEqual(test, "Unexpected request body", `{"humanReadable":"This event is from the bulk test","source":"metadata_api_test","type":"bulkTestEvent"}`, latestRequestBody)
 }
 
 func assertEqual(test *testing.T, message string, expected interface{}, actual interface{}) {
