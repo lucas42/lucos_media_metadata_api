21f2536e474c7486b9e71b4b7798fb4a15ebd5c5
Luke Blaney <git@lukeblaney.co.uk>
2024-02-25 14:08:09 +0000

    add additional tests for weightings in collections to match tests for normal weightings logic

--- src/tracks_weighting_test.go
+++ src/tracks_weighting_test.go
@@ -133,3 +133,55 @@ func TestZeroWeightingAfterDelete(test *testing.T) {
 	assertWeighting(test, store, 4, 5, 10)
 	assertWeighting(test, store, 3, 0, 0)
 }
+
+func assertCollectionWeighting(test *testing.T, store Datastore, trackid int, collectionslug string, expectedWeighting float64, expectedCumulativeWeighting float64) {
+	actualWeighting, err := store.getTrackWeighting(trackid)
+	if err != nil {
+		test.Errorf("Error getting weighting for track %d: %s", trackid, err.Error())
+	}
+	actualCumulativeWeighting := 0.0
+	err = store.DB.Get(&actualCumulativeWeighting, "SELECT cum_weighting FROM collection_track WHERE trackid=$1 AND collectionslug=$2", trackid, collectionslug)
+	if err != nil {
+		test.Errorf("Error getting cumulative weighting for track %d: %s", trackid, err.Error())
+	}
+	assertEqual(test, "Incorrect Weighting for track "+strconv.Itoa(trackid), expectedWeighting, actualWeighting)
+	assertEqual(test, "Incorrect Cumulative Weighting for track "+strconv.Itoa(trackid), expectedCumulativeWeighting, actualCumulativeWeighting)
+}
+
+func TestDeletingTrackCorrectsWeightingInCollection(test *testing.T) {
+	clearData()
+	store := DBInit("testweighting.sqlite", MockLoganne{})
+	_, err := store.DB.Exec("INSERT INTO track (url,fingerprint,duration) values ('/track1','abc',3),('/track2','def',6),('/track3','hij',9)")
+	if err != nil {
+		test.Errorf("Error inserting tracks: %s", err.Error())
+	}
+	_, err = store.DB.Exec("INSERT INTO collection (slug,name) values ('main-collection','Main')")
+	if err != nil {
+		test.Errorf("Error inserting collection: %s", err.Error())
+	}
+	_, err = store.DB.Exec("INSERT INTO collection_track (collectionslug, trackid) values ('main-collection',1),('main-collection',2),('main-collection',3)")
+	if err != nil {
+		test.Errorf("Error inserting collection_track: %s", err.Error())
+	}
+	err = store.setTrackWeighting(1, 5)
+	if err != nil {
+		test.Errorf("Error setting weighting 1: %s", err.Error())
+	}
+	err = store.setTrackWeighting(2, 5)
+	if err != nil {
+		test.Errorf("Error setting weighting 2: %s", err.Error())
+	}
+	err = store.setTrackWeighting(3, 5)
+	if err != nil {
+		test.Errorf("Error setting weighting 3: %s", err.Error())
+	}
+	assertCollectionWeighting(test, store, 1, "main-collection", 5, 5)
+	assertCollectionWeighting(test, store, 2, "main-collection", 5, 10)
+	assertCollectionWeighting(test, store, 3, "main-collection", 5, 15)
+	err = store.deleteTrack(2)
+	if err != nil {
+		test.Errorf("Error deleting track 2: %s", err.Error())
+	}
+	assertCollectionWeighting(test, store, 1, "main-collection", 5, 5)
+	assertCollectionWeighting(test, store, 3, "main-collection", 5, 10)
+}
